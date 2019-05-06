9afd89bd973b7f8e1d04c2e285f1a6849cd0dc3c
Carlos Rhedney <twisterpsa@hotmail.com>
2019-05-05 23:40:54 -0300

    Add Correcao

--- Projeto #woOhoO/index.php
+++ Projeto #woOhoO/index.php
@@ -58,11 +58,11 @@
     <div class="modal fade" id="modal">
       <div class="modal-dialog">
         <div style="background-color:#E0FFFF;" class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4>Efetuar login</h4>
-          </div>
           <form role="form" action="verifica_login.php" method="post">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Efetuar login</h4>
+            </div>
             <div class="modal-body">
               <div class="form-group">
                 <input type="email" class="form-control" name="email" placeholder="Digite seu email..." required />
@@ -70,10 +70,10 @@
               <div class="form-group">
                 <input type="password" class="form-control" name="senha" placeholder="Digite sua senha..." required />
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                <button type="submit" class="btn btn-primary">Entrar</button>
-              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+              <button type="submit" class="btn btn-primary">Entrar</button>
             </div>
           </form>
         </div>
