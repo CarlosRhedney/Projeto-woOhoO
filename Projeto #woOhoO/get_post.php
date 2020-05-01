012a75d0f283dd9dfbea31b3b0ad6e62f69bc5d9
Carlos Rhedney <twisterpsa@hotmail.com>
2020-05-01 02:37:39 -0300

    Add Remove Button

--- Projeto #woOhoO/get_post.php
+++ Projeto #woOhoO/get_post.php
@@ -7,12 +7,16 @@ include("conexao_db.php");
 $id_usuario = $_SESSION["id_usuario"];
 $con = new db();
 $conexao = $con->conecta_mysql();
-$sql = " SELECT DATE_FORMAT(p.data_inclusao, '%d %b %Y %T') AS data_inclusao_format, p.post, t.usuario, t.id FROM post AS p JOIN tb_usuarios AS t ON (p.id_usuario = t.id) WHERE id_usuario = $id_usuario OR id_usuario IN ( SELECT seguindo_id_usuario FROM usuarios_seguidores WHERE id_usuario = $id_usuario) ORDER BY data_inclusao DESC ";
+$sql = " SELECT DATE_FORMAT(p.data_inclusao, '%d %b %Y %T') AS data_inclusao_format, p.post, p.id_post, t.usuario, t.id FROM post AS p JOIN tb_usuarios AS t ON (p.id_usuario = t.id) WHERE id_usuario = $id_usuario OR id_usuario IN ( SELECT seguindo_id_usuario FROM usuarios_seguidores WHERE id_usuario = $id_usuario) ORDER BY data_inclusao DESC ";
 $resultado_id = mysqli_query($conexao, $sql);
 if($resultado_id){
 	while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
 		echo '<a href="#" class="list-group-item">';
 		echo '<h4 class="list-group-item-heading">'.$registro['usuario'].' <small> - '.$registro['data_inclusao_format'].'</small></h4>';
+		echo "<p class='list-group-item-text pull-right'>";
+		echo "<button type='button' style='border:none' class='btn btn-default glyphicon glyphicon-trash btn_del' data-id_post='".$registro["id_post"]."'></button>";
+		echo "<div class='clearfix'></div>";
+		echo "</p>";
 		echo "<br />";
 		echo '<p class="list-group-item-text"><i><font style="font-size:17px">'.$registro['post'].'</i></p>';		
 		echo '<hr />';
